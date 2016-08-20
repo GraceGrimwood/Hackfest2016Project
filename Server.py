1d58eeef772cd5718c48aef58cd329bdc0a9a5b9
Grace Grimwood <ravensandfishcustard@gmail.com>
2016-08-20 13:01:39 +1200

    Server works now

--- Server.py
+++ Server.py
@@ -1,4 +1,7 @@
-from bottle import static_file, get, post, request
+from bottle import route, run, static_file, get, post, request
 
-def static_path = "static\"
-
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root = 'static\\')
+	
+run(host = 'localhost', port = 8080, debug = True)
