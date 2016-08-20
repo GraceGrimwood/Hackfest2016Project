d8f00edcc8808a1cf513eff32270feebff4ffcc6
MariaAlicia <maria.ahkit@hotmail.com>
2016-08-21 10:07:03 +1200

    started implementation of search box. It is there and you can type but it does not zoom it or allow you to actually enter anything.

---
+++ static/js/search.js
@@ -0,0 +1,17 @@
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('search');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+  }
