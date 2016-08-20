97fec133c8ac9d30eeaabeea4159f4082ce0f019
Grace Grimwood <ravensandfishcustard@gmail.com>
2016-08-20 13:06:40 +1200

    Have a redirect to index now
    For non-python peeps, run 'python Server.py' from command line

--- Server.py
+++ Server.py
@@ -1,7 +1,12 @@
-from bottle import route, run, static_file, get, post, request
+from bottle import route, run, static_file, get, post, request, redirect
+
+@route('/')
+def route_to_index():
+	redirect('static/index.html')
 
 @route('/static/<filename>')
 def server_static(filename):
     return static_file(filename, root = 'static\\')
+
 	
 run(host = 'localhost', port = 8080, debug = True)
