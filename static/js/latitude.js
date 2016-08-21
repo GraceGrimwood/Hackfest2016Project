ee19ca74cf05063702e71648120f68097650318f
sairam1244 <sairam.nimmagadda@gmail.com>
2016-08-21 13:12:45 +1200

    It is displaying circles by using static json data.

--- static/js/latitude.js
+++ static/js/latitude.js
@@ -1,36 +1,31 @@
-					$(document).ready(function() {
+$(document).ready(function() {
+	var suburbPositions=[];
+	$.getJSON("/static/js/SuburbsLatlon.json", function(data){
+		suburbPositions=data;
+		initialize(suburbPositions);
+	});
+});	
+function initialize(suburbLatLngs) {	
+	var mapProp = {
+		center:suburbLatLngs[0].center,
+		zoom:7,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};  
+	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+	for(i=0;i<suburbLatLngs.length;i++){	
+		var myCity = new google.maps.Circle({
+		  center:suburbLatLngs[i].center,
+		  radius:3000,
+		  strokeColor:"#0000FF",
+		  strokeOpacity:0.3,
+		  strokeWeight:2,
+		  fillColor:"#0000FF",
+		  fillOpacity:0.4
+		});
+	
+		myCity.setMap(map);
+	}
+}
+	
+	
 
-					// url to find latitude and longitude based on suburbs.
-					var url="https://maps.googleapis.com/maps/api/geocode/json?address=Chertsey,NZ&key=AIzaSyB_g5IBb2LXiYkRRWnjZfLGivlXG-xZ-Fc";
-
-					$.getJSON(url, function (response) {
-						
-						var latitude=response.results[0].geometry.location.lat;
-						var longitude=response.results[0].geometry.location.lng;
-						var amsterdam=new google.maps.LatLng(latitude,longitude);
-						function initialize()
-						{
-						var mapProp = {
-						  center:amsterdam,
-						  zoom:7,
-						  mapTypeId:google.maps.MapTypeId.ROADMAP
-						  };
-						  
-						var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-						var myCity = new google.maps.Circle({
-						  center:amsterdam,
-						  radius:20000,
-						  strokeColor:"#0000FF",
-						  strokeOpacity:0.8,
-						  strokeWeight:2,
-						  fillColor:"#0000FF",
-						  fillOpacity:0.4
-						  });
-
-						myCity.setMap(map);
-						}
-						//google.maps.event.addDomListener(window, 'load', initialize);
-						initialize();
-					});
-					});
