273fba8f1ddef4f32fabdaaa54c61161a41ee762
MariaAlicia <maria.ahkit@hotmail.com>
2016-08-21 12:42:57 +1200

    Made the changes to make the map global across files. Search bar has full functionality including autocomplete and zoom in for the map

--- static/js/latitude.js
+++ static/js/latitude.js
@@ -1,82 +1,9 @@
-					$(document).ready(function() {
+					//$(document).ready(function() {
 
 					// url to find latitude and longitude based on suburbs.
-					var url="https://maps.googleapis.com/maps/api/geocode/json?address=Chertsey,NZ&key=AIzaSyB_g5IBb2LXiYkRRWnjZfLGivlXG-xZ-Fc";
-
-					$.getJSON(url, function (response) {
-						
-						var latitude=response.results[0].geometry.location.lat;
-						var longitude=response.results[0].geometry.location.lng;
-						var place=new google.maps.LatLng(latitude,longitude);
-						function initialize()
+						function initialize(map)
 						{
-						var mapProp = {
-						  center:place,
-						  zoom:7,
-						  //mapTypeId:google.maps.MapTypeId.ROADMAP,
-						  styles: [[{featureType:"administrative",
-							elementType:"all",
-							stylers:[{visibility:"on"}]
-							},{
-							featureType:"landscape",
-							elementType:"all",
-							stylers:[{hue:"#ff0300"},
-									 {saturation:-100},
-									 {lightness:129.33333333333334},
-									 {gamma:1},
-									 {visibility:"on"}]
-							},{
-							featureType:"poi",
-							elementType:"all",
-							stylers:[{hue:"#abff00"},
-									 {saturation:61.80000000000001},
-									 {lightness:13.800000000000011},
-									 {gamma:1},
-									 {visibility:"on"}]
-							},{
-							featureType:"road",
-							elementType:"all",
-							stylers:[{visibility:"off"}]
-							},{
-							featureType: "road.highway",
-							elementType: "labels.icon",
-							stylers: [{visibility: "off"}]
-							},{
-							featureType: "road.arterial",
-							elementType: "all",
-							stylers: [{hue: "#00b7ff"},
-									  {saturation: -31.19999999999996},
-									  {lightness: 2.1803921568627374},
-									  {gamma: 1},
-									  {visibility: "on"}]
-							},{
-							featureType: "road.local",
-							elementType: "all",
-							stylers: [{hue: "#00B5FF"},
-									  {saturation: -33.33333333333343},
-									  {lightness: 27.294117647058826},
-									  {gamma: 1},
-									  {visibility: "on"}]
-							},{
-							featureType:"transit.station.airport",
-							elementType:"all",
-							stylers:[{visibility:"on"}]
-							},{
-							featureType:"transit.station.bus",
-							elementType:"all",
-							stylers:[{visibility:"off"}]
-							},{
-							featureType:"water",
-							elementType:"all",
-							stylers:[{hue:"#00B7FF"},
-									 {saturation:8.400000000000006},
-									 {lightness:36.400000000000006},
-									 {gamma:1}]}
-							]]
-						  };
-						  
-						var map = new google.maps.Map(document.getElementById("map"),mapProp);
-
+						
 						var myCity = new google.maps.Circle({
 						  center:place,
 						  radius:20000,
@@ -92,4 +19,4 @@
 						//google.maps.event.addDomListener(window, 'load', initialize);
 						initialize();
 					});
-					});
+					//});
--- static/js/search.js
+++ static/js/search.js
@@ -1,9 +1,9 @@
-function initAutocomplete() {
-  var map = new google.maps.Map(document.getElementById('map'), {
+function initAutocomplete(map) {
+  /*var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -33.8688, lng: 151.2195},
     zoom: 13,
     mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
+  });*/
 
   // Create the search box and link it to the UI element.
   var input = document.getElementById('search');
