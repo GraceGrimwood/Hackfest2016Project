67f1abdf0c618a45a859f50a751c30f4eb3eadd2
MariaAlicia <maria.ahkit@hotmail.com>
2016-08-21 13:53:47 +1200

    Integrated latitude.js with main map code.

--- static/js/latitude.js
+++ static/js/latitude.js
@@ -1,30 +1,22 @@
-$(document).ready(function() {
+function initialize(map) {	
+	console.log(map);
 	var suburbPositions=[];
 	$.getJSON("/static/js/SuburbsLatlon.json", function(data){
 		suburbPositions=data;
-		initialize(suburbPositions);
-	});
-});	
-function initialize(suburbLatLngs) {	
-	var mapProp = {
-		center:suburbLatLngs[0].center,
-		zoom:7,
-		mapTypeId:google.maps.MapTypeId.ROADMAP
-	};  
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	for(i=0;i<suburbLatLngs.length;i++){	
-		var myCity = new google.maps.Circle({
-		  center:suburbLatLngs[i].center,
-		  radius:3000,
-		  strokeColor:"#0000FF",
-		  strokeOpacity:0.3,
-		  strokeWeight:2,
-		  fillColor:"#0000FF",
-		  fillOpacity:0.4
-		});
-	
-		myCity.setMap(map);
-	}
+		for(i=0;i<suburbPositions.length;i++){	
+			var myCity = new google.maps.Circle({
+			  center:suburbPositions[i].center,
+			  radius:3000,
+			  strokeColor:"#0000FF",
+			  strokeOpacity:0.3,
+			  strokeWeight:2,
+			  fillColor:"#0000FF",
+			  fillOpacity:0.4
+			});
+		
+			myCity.setMap(map);
+		}
+	}); 
 }
 	
 	
