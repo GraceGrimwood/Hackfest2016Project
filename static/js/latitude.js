6f8ea6ea33a7d062eae9888b60272cc46cfd0651
Grace Grimwood <ravensandfishcustard@gmail.com>
2016-08-23 20:18:30 +1200

    Fixed merge bugs

--- static/js/latitude.js
+++ static/js/latitude.js
@@ -1,38 +1,6 @@
 function initialize(curmap) {	
 	console.log(curmap);
 	var suburbPositions=[];
-<<<<<<< HEAD
-	$.getJSON("/api/region/Ashburton/colourmap.json", function(data){
-		
-		suburbPositions=data;
-		for(i=0;i<suburbPositions.length;i++){
-			var myCity = new google.maps.Circle({
-			  center:suburbPositions[i].center,
-			  radius:2000,
-			  strokeColor: rgbtohex(suburbPositions[i].Color),
-			  strokeOpacity:0.3,
-			  strokeWeight:2,
-			  fillColor:"#0000FF",
-			  fillOpacity:0.4
-			});
-		
-			myCity.setMap(map);
-		}
-	}); 
-}
-
-function rgbtohex(rgbval){
-	console.log(rgbval);
-	var red = rgbval.split(",")[0];
-	var green=rgbval.split(",")[1];
-	var blue=rgbval.split(",")[2];
-	return "#" +componenttohex(red) + componenttohex(green) + componenttohex(blue);
-}
-
-function componenttohex(c){
-	var hex = c.toString(16);
-	return hex.length===1 ? '0' + hex : hex;
-=======
 	$.getJSON("/api/regions/colourmap.json", function(data){
 		suburbPositions=data;
 		for(i=0;i<suburbPositions.length;i++){	
@@ -50,5 +18,4 @@ function componenttohex(c){
 			}
 		}
 	}); 
->>>>>>> master
 }
