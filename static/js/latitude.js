707ff62d334a896df915836fff436226711cdb16
Grace Grimwood <ravensandfishcustard@gmail.com>
2016-08-23 20:16:03 +1200

    I GOT IT TO WORK!!! HOUSTON WE HAVE COLOUR!!!

--- static/js/latitude.js
+++ static/js/latitude.js
@@ -1,35 +1,21 @@
-function initialize(map) {	
-	console.log(map);
+function initialize(curmap) {	
+	console.log(curmap);
 	var suburbPositions=[];
-	$.getJSON("/static/js/SuburbsLatlon.json", function(data){
+	$.getJSON("/api/regions/colourmap.json", function(data){
 		suburbPositions=data;
 		for(i=0;i<suburbPositions.length;i++){	
-			var myCity = new google.maps.Circle({
-			  center:suburbPositions[i].center,
-			  radius:2000,
-			  strokeColor:"#0000FF",
-			  strokeOpacity:0.3,
-			  strokeWeight:2,
-			  fillColor:"#0000FF",
-			  fillOpacity:0.4
-			});
-		
-			myCity.setMap(map);
+			for(j=0;j<suburbPositions[i].length;j++){
+				var myCity = new google.maps.Circle({
+					center:suburbPositions[i][j].center,
+					radius:2000,
+					strokeColor: suburbPositions[i][j].Color,
+					strokeOpacity:0.3,
+					strokeWeight:2,
+					fillColor: suburbPositions[i][j].Color,
+					fillOpacity:0.4
+				});
+				myCity.setMap(curmap);
+			}
 		}
 	}); 
-}
-
-function rgbtohex(rgbval){
-	red = rgbval.split(",")[0];
-	green=rgbval.split(",")[1];
-	blue=rgbval.split(",")[2];
-	return "#" +componenttohex(red) + componenttohex(green) + componenttohex(blue);
-}
-
-function componenttohex(c){
-	var hex = c.toString(16);
-	return hex.length===1 ? '0' + hex : hex;
-	
-	
-	
 }
